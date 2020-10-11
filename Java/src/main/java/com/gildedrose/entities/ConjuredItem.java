175b32c69e5c49d6ca5d002301c3b40a60177492
Delvin Low <delvinlowzy@gmail.com>
2020-10-11 18:15:26 +0800

    Add test to decrement quality twice as fast after sell in for conjured item

---
+++ Java/src/main/java/com/gildedrose/entities/ConjuredItem.java
@@ -0,0 +1,21 @@
+package com.gildedrose.entities;
+
+public class ConjuredItem extends TypedItem {
+    public ConjuredItem(Item item) {
+        super(item.name, item.sellIn, item.quality);
+    }
+
+    public void updateQuality() {
+        int decrementBy;
+        if (this.sellIn < 0) {
+            decrementBy = 4;
+        } else {
+            decrementBy = 2;
+        }
+        this.quality = Math.max(0, this.quality - decrementBy);
+    }
+
+    public void updateSellIn() {
+        this.sellIn = this.sellIn - 1;
+    }
+}
--- Java/src/test/java/com/gildedrose/ConjuredItemTest.java
+++ Java/src/test/java/com/gildedrose/ConjuredItemTest.java
@@ -17,4 +17,15 @@ public class ConjuredItemTest {
         assertEquals(2, app.items[0].sellIn);
         assertEquals(4, app.items[0].quality);
     }
+
+    @Test
+    void shouldDecrementQualityByFourDailyAfterSellInDateForConjuredItems() {
+        Item[] items = new Item[] {
+                new Item("Conjured Mana Cake", 0, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+    }
 }
